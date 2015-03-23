c5ae2685e555f1e0fddf8a4f17c1e58e352337c8
KarlJoones- <C13315991@mydit.ie>
2015-03-23 22:12:56 +0000

    Initial version

---
+++ src/ie/Caesar.java
@@ -0,0 +1,12 @@
+package ie;
+
+/* This program is used to encrypt and decrypt using the caesar shift algorithm
+ * Began: 23 March 2015
+ * Copyright Karl Jones
+ */
+
+public class Caesar {
+	public static void main(String[] args) {
+		
+	} // End main
+} // End of class
