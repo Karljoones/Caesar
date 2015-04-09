c33506ef4fa673d5583010a6b2affa1662a1e349
KarlJoones- <C13315991@mydit.ie>
2015-04-09 16:25:47 +0100

    Added Cipher.java

---
+++ src/ie/Cipher.java
@@ -0,0 +1,14 @@
+package ie;
+
+public class Cipher {
+	public static String encode(String original, String key){
+		
+		// Variables
+		String encoded;
+		
+		int Key = Integer.parseInt(key);
+		Key = Key % 26 + 26;
+		
+		return encoded;
+	}
+}
