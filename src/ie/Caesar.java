12948f7396a936c90896dc2d8caf9555996571d8
KarlJoones- <C13315991@mydit.ie>
2015-04-09 16:31:56 +0100

    Added encode()

--- src/ie/Caesar.java
+++ src/ie/Caesar.java
@@ -8,22 +8,13 @@ package ie;
 public class Caesar {
 	public static void main(String[] args) {
 		
+		String original;
+		String key;
+		
 		// Get the user to enter in the string and the key they want to use
 		System.out.println("Please enter the string you want to encrypt:\n");			// get string
 		System.out.println("Please enter the key you want to excrypt \"%s\" with:\n");	// get key
+		System.out.println(Cipher.encode(original, key));
 	} // End main
 	
-	// This method is to encrypt the string with the chosen key
-	public String encrypt(String original, Integer key) {
-		String encrypted = "World";
-		
-		return encrypted;
-	} // End encrypt()
-	
-	// This method is to decrypt the string with the chosen key, only works if
-	public String decrypt(String encrypted, Integer key) {
-		String decrypted = "Hello";
-		
-		return decrypted;
-	} // End decrypt()
 } // End of class
--- src/ie/Cipher.java
+++ src/ie/Cipher.java
@@ -1,14 +1,27 @@
 package ie;
 
 public class Cipher {
+	
+	// Method to encode the data
 	public static String encode(String original, String key){
-		
-		// Variables
-		String encoded;
 		
 		int Key = Integer.parseInt(key);
 		Key = Key % 26 + 26;
 		
-		return encoded;
+		StringBuilder encoded = new StringBuilder();
+		
+		for ( char i : original.toCharArray() ) {
+			if ( Character.isLetter(i) ) {
+				if( Character.isUpperCase(i) ) {
+					encoded.append( (char) ('A' + ( i - 'A' + Key ) % 26 ) );
+				} else {
+					encoded.append( (char) ('a' + ( i - 'a' + Key ) % 26 ) );
+				}
+			} else {
+				encoded.append(i);
+			}
+		}
+		
+		return encoded.toString();
 	}
 }
