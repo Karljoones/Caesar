2fbe430d558c901c3e3aae18ce8d5395c1064dbd
KarlJoones- <C13315991@mydit.ie>
2015-04-14 12:47:09 +0100

    GUI

--- src/ie/Caesar.java
+++ src/ie/Caesar.java
@@ -1,6 +1,7 @@
 package ie;
 
 import java.io.*;
+import javax.swing.JFrame;
 
 /* This program is used to encrypt and decrypt using the caesar shift algorithm
  * Began: 23 March 2015
@@ -14,42 +15,46 @@ public class Caesar {
 	private static String key = "12";
 	
 	public static void main(String[] args) {
+	
+//		// Get the user to enter in the string and the key they want to use
+//		System.out.println("Please enter the string you want to encrypt:\n");			// get string
+//		BufferedReader str = new BufferedReader(new InputStreamReader(System.in));
+//		try {
+//			Caesar.setOriginal(str.readLine());
+//		} catch (IOException e) {
+//			System.out.println("IO error trying to read in string.");
+//			System.exit(1);
+//		}
+//		
+//		// Get the offset that the user wants to offset the string with
+//		System.out.println("Please enter the key you want to encrypt \"" + Caesar.getOriginal() + "\" with:\n");	// get key
+//		BufferedReader ky = new BufferedReader(new InputStreamReader(System.in));
+//		try {
+//			Caesar.setKey(ky.readLine());
+//		} catch (IOException e) {
+//			System.out.println("IO error trying to read in key.");
+//			System.exit(1);
+//		}
+//		
+//		int Key = 0;
+//		
+//		// Convert the String key into an integer Key.
+//		try {
+//			Key = Integer.parseInt(Caesar.getKey());
+//		} catch (NumberFormatException e) {
+//			System.out.println("IO error in format of number.");
+//			System.exit(1);
+//		}
+//		
+//		// Print out the encoded data and the original message under
+//		System.out.println("Your encoded string is: " + Cipher.removeSpaces(Cipher.encode(Caesar.getOriginal(), Key)));
+//		System.out.println("Your original string was: " + Cipher.decode(Cipher.encode(Caesar.getOriginal(), Key), Key));
+
+		GUI gridBagFrame = new GUI();
+		gridBagFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gridBagFrame.setSize(300,150);
+		gridBagFrame.setVisible(true);
 		
-		// Get the user to enter in the string and the key they want to use
-		System.out.println("Please enter the string you want to encrypt:\n");			// get string
-		BufferedReader str = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			Caesar.setOriginal(str.readLine());
-		} catch (IOException e) {
-			System.out.println("IO error trying to read in string.");
-			System.exit(1);
-		}
-		
-		// Get the offset that the user wants to offset the string with
-		System.out.println("Please enter the key you want to encrypt \"" + Caesar.getOriginal() + "\" with:\n");	// get key
-		BufferedReader ky = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			Caesar.setKey(ky.readLine());
-		} catch (IOException e) {
-			System.out.println("IO error trying to read in key.");
-			System.exit(1);
-		}
-		
-		int Key = 0;
-		
-		// Convert the String key into an integer Key.
-		try {
-			Key = Integer.parseInt(Caesar.getKey());
-		} catch (NumberFormatException e) {
-			System.out.println("IO error in format of number.");
-			System.exit(1);
-		}
-		
-		String encoded = Cipher.encode(Caesar.getOriginal(), Key);
-		
-		// Print out the encoded data and the original message under
-		System.out.println("Your encoded string is: " + Cipher.removeSpaces(encoded));
-		System.out.println("Your original string was: " + Cipher.decode(Cipher.encode(Caesar.getOriginal(), Key), Key));
 	} // End main
 	
 	// Accessors
--- src/ie/GUI.java
+++ src/ie/GUI.java
@@ -6,6 +6,7 @@ package ie;
  */
 
 import java.awt.*;
+
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -14,20 +15,64 @@ import javax.swing.JComboBox;
 
 // This class is in control of the user interface.
 public class GUI extends JFrame{
+	private static final long serialVersionUID = 1L;
 	private GridBagLayout layout;
 	private GridBagConstraints constraints;
 	
 	// set up the GUI
-	public GridBagLayout() {
+	public GUI() {
 		super("Caesar");
 		setLayout(layout);
 		constraints = new GridBagConstraints();
 		
 		// Create components
 		JTextArea textArea1 = new JTextArea("Input", 5, 10);
-		JTextArea textarea2 = new JTextArea("Output", 2, 2);
+		JTextArea textArea2 = new JTextArea(" ", 2, 2);
 		
-		String[] keys = {"1", "2", "3", "4"};
+		String[] keys = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"};
+		JComboBox comboBox = new JComboBox(keys);
+		
+		JTextField textField = new JTextField("Output");
+		JButton button1 = new JButton("Encode");
+		JButton button2 = new JButton("Decode");
+		JButton button3 = new JButton("Help");
+		
+		// Add text area 1
+		constraints.fill = GridBagConstraints.BOTH;
+		addComponent(textArea1, 0, 0, 1, 3);
+		
+		// Add button 1
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		addComponent(button1, 0, 1, 2, 1);
+		
+		// Add the combobox
+		addComponent(comboBox, 2, 1, 2, 1);
+		
+		// Add button 2
+		constraints.weightx = 1000;
+		constraints.weighty = 1;
+		constraints.fill = GridBagConstraints.BOTH;
+		addComponent(button2, 1, 1, 1, 1);
+		
+		// Add button 3
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+		addComponent(button3, 1, 2, 1, 1);
+		
+		// Add text field
+		addComponent(textField, 3, 0, 2, 1);
+		
+		// Add text area 2
+		addComponent(textArea2, 3, 2, 1, 1);
 		
 	} // End constructor
+	
+	private void addComponent(Component component, int row, int column, int width, int height) {
+		constraints.gridx = column;
+		constraints.gridy = row;
+		constraints.gridwidth = width;
+		constraints.gridheight = height;
+		layout.setConstraints(component, constraints);
+		add(component);
+	} // End method
 }
