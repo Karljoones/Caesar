e6fc4896d94c6ff2babd0b10865a1c3b12806cb0
KarlJoones- <C13315991@mydit.ie>
2015-04-15 13:04:18 +0100

    More information for users

--- src/ie/Caesar.java
+++ src/ie/Caesar.java
@@ -51,29 +51,33 @@ public class Caesar {
 //			System.out.println("IO error in format of number.");
 //			System.exit(1);
 //		}
-//		
-
 		
-		// This tells the user how to use the program.
+
+		// Give the user information on how to use the program
 		JOptionPane.showMessageDialog(null, "Welcome!"
-				+ "\nTo encode, please enter the string and then you will be asked for the key."
+				+ "\nTo encode, enter the string and then you will be asked for the key."
 				+ "\nTo decode, enter the encoded string, simply take your key away from 26 to decode the message."
 				+ "\nPlease note: this does not work with numerical values."
 				+ "\nTo exit, simply type \"exit\".", 
-				"How to", 
+				"How to use the program", 
 				JOptionPane.PLAIN_MESSAGE);
 		
 		// Get the user inputs
-		Caesar.setOriginal(JOptionPane.showInputDialog("Enter the string to encode:"));
-		if(Caesar.getOriginal().equals("exit")) {
+		Caesar.setOriginal(JOptionPane.showInputDialog("Enter the string you want to encode:"));
+		
+		// This code is used as an escape for the user, typing exit into the window exits the program.
+		if(Caesar.getOriginal().equals("exit") || Caesar.getOriginal().equals("Exit") || Caesar.getOriginal().equals("EXIT")) {
 			System.exit(1);
 		}
-		Caesar.setKey(JOptionPane.showInputDialog("Enter the key to encode:"));
-		if(Caesar.getKey().equals("exit")){
+		Caesar.setKey(JOptionPane.showInputDialog("Enter the key to encode / decode:"));
+		
+		// This code is used as an escape for the user, typing exit into the window exits the program.
+		if(Caesar.getKey().equals("exit") || Caesar.getKey().equals("Exit") || Caesar.getKey().equals("EXIT")){
 			System.exit(1);
 		}
 		
 		// This code copies the encoded string into the clipboard so that the user can paste it, or reopen the program and paste it back in (used for decoding the message)
+		// The sequence of things is that the original is gotten, encoded then sent to get anything that is not a character removed.
 		StringSelection stringSelection = new StringSelection(Cipher.removeSpaces(Cipher.encode(Caesar.getOriginal(), 
 				Caesar.changeKey(Caesar.getKey()))));
 		Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -82,14 +86,22 @@ public class Caesar {
 		// Show the user the output and inform them of the text that was copied to the clipboard and what the decoding key is.
 		JOptionPane.showMessageDialog(null, 
 				Cipher.removeSpaces(Cipher.encode(Caesar.getOriginal(), Caesar.changeKey(Caesar.getKey()))) 
-				+ "\nThis has been copied into your clipboard.\n"
-				+ "Your decoding key is: " + (26 - Caesar.changeKey(Caesar.getKey())), 
+				+ "\nThis has been copied into your clipboard."
+				+ "\nYour decoding key is: " + (26 - Caesar.changeKey(Caesar.getKey())
+				+ "\nYour encoding key is: " + Caesar.changeKey(Caesar.getKey())), 
 				"Encoded", 
 				JOptionPane.PLAIN_MESSAGE);
 		
 		// Print out the encoded data and the original message under after the program has ended
-		System.out.println("Your encoded string is: " + Cipher.removeSpaces(Cipher.encode(Caesar.getOriginal(), Caesar.changeKey(Caesar.getKey()))));
-		System.out.println("Your original string was: " + Cipher.decode(Cipher.encode(Caesar.getOriginal(), Caesar.changeKey(Caesar.getKey())), Caesar.changeKey(Caesar.getKey())));
+		System.out.println("Your encoded string is: " 
+				+ Cipher.removeSpaces(Cipher.encode(Caesar.getOriginal(), 
+				Caesar.changeKey(Caesar.getKey()))));
+		System.out.println("Your original string was: " 
+					+ Cipher.decode(Cipher.encode(Caesar.getOriginal(), 
+					Caesar.changeKey(Caesar.getKey())), 
+					Caesar.changeKey(Caesar.getKey())));
+		System.out.println("Your decoding key is: " + (26 - Caesar.changeKey(Caesar.getKey())));
+		System.out.println("Your encoding key is: " + Caesar.changeKey(Caesar.getKey()));
 	} // End main
 	
 	// This is used to change the key from a string to an int
@@ -99,7 +111,7 @@ public class Caesar {
 		try {
 			Key = Integer.parseInt(Caesar.getKey());
 		} catch (NumberFormatException e) {
-			System.out.println("IO error in format of number.");
+			System.out.println("IO error in the format of the number, please make sure it is between 0-9");
 			System.exit(1);				// End the program
 		}
 		return Key;
